7152795cb9b0eb425397d2309f58519548b60d72
Sunbright53 <141810435+Sunbright53@users.noreply.github.com>
2024-05-17 13:53:22 +0700

    feat: save file

--- ex-2.js
+++ ex-2.js
@@ -1,15 +1,34 @@
-//Exercise #2: At Least Five Function
-
 function atLeastFive(array, operation) {
-  // Start coding here
+  let count = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (operation(array[i])) {
+      count++;
+    }
+    if (count >= 5) {
+      return true;
+    }
+  }
+  return false;
 }
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
 const studentScoresRoom2 = [78, 98, 23, 15, 40, 12, 40, 67, 80, 100];
 const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
-// Using `atLeastFive` function here
+function isGreaterThan70(score) {
+  return score > 70;
+}
 
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+let scoreRoom1Result = atLeastFive(studentScoresRoom1, isGreaterThan70)
+  ? "ผ่านเกณฑ์ ✅"
+  : "ไม่ผ่านเกณฑ์ ❌";
+let scoreRoom2Result = atLeastFive(studentScoresRoom2, isGreaterThan70)
+  ? "ผ่านเกณฑ์ ✅"
+  : "ไม่ผ่านเกณฑ์ ❌";
+let scoreRoom3Result = atLeastFive(studentScoresRoom3, isGreaterThan70)
+  ? "ผ่านเกณฑ์ ✅"
+  : "ไม่ผ่านเกณฑ์ ❌";
+
+console.log(`นักเรียนห้องที่ 1 ${scoreRoom1Result}`);
+console.log(`นักเรียนห้องที่ 2 ${scoreRoom2Result}`);
+console.log(`นักเรียนห้องที่ 3 ${scoreRoom3Result}`);
